7a692d306c8bac66e164e889bcf479885cb7d4f7
millenium200 <moonmasters200@gmail.com>
2014-10-03 20:28:31 -0400

    many formatting changes, some more cases filled

--- src/main/java/com/gmail/moonmasters200/RaffleTickets/Main.java
+++ src/main/java/com/gmail/moonmasters200/RaffleTickets/Main.java
@@ -10,11 +10,14 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin
+{
 	
 	@SuppressWarnings("deprecation")
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("raffles")) {
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	{
+		if (cmd.getName().equalsIgnoreCase("raffles"))
+		{
 			sender.sendMessage(ChatColor.GOLD + "[Swag-Raffles] " + ChatColor.BLUE + "Raffles Plugin by " + ChatColor.RED + "millenium200 " +
 			ChatColor.BLUE + "for Swag-Craft Factions Reborn!");
 			sender.sendMessage(ChatColor.GOLD + "[Swag-Raffles] " + ChatColor.BLUE + "Buy a raffle ticket with " + ChatColor.GREEN +
@@ -23,12 +26,15 @@ public class Main extends JavaPlugin {
 			return true;
 		}
 		
-		if (cmd.getName().equalsIgnoreCase("raffle")) {
-			if (args.length != 1) {
+		if (cmd.getName().equalsIgnoreCase("raffle")) 
+		{
+			if (args.length != 1)
+			{
 				//When onCommand() returns false, the help message associated with the
 				//command is displayed.
 				return false;
-			} else if (!(sender instanceof Player)) { //Make sure the sender is a player.
+			} else if (!(sender instanceof Player)) 
+			{ //Make sure the sender is a player.
 				sender.sendMessage("Only players can buy raffle tickets!");
 				return true;
 			}
@@ -37,244 +43,361 @@ public class Main extends JavaPlugin {
 			
 			int randomNumber;
 			ItemStack itemstack;
-			String prizeName;
+			String prizeName = null;
 			
 			randomNumber = (int)(Math.random() * 1000);
 			randomNumber = 6;
-			switch(randomNumber) {
-			case 1: {
+			switch(randomNumber) 
+			{
+			case 1:
+			{
 				itemstack = new ItemStack(Material.OBSIDIAN, 16);
 				prizeName = "Obsidian";
 				PlayerInventory inventory = player.getInventory();
 				inventory.addItem(itemstack);
 				break;
 			}
-			case 2: {
+			case 2:
+			{
 				itemstack = new ItemStack(Material.OBSIDIAN, 16);
 				prizeName = "Obsidian";
 				PlayerInventory inventory = player.getInventory();
 				inventory.addItem(itemstack);
 				break;
 			}
-			case 3: {
+			case 3:
+			{
 				itemstack = new ItemStack(Material.OBSIDIAN, 16);
 				prizeName = "Obsidian";
 				PlayerInventory inventory = player.getInventory();
 				inventory.addItem(itemstack);
 				break;
 			}
-			case 4: {
+			case 4:
+			{
 				itemstack = new ItemStack(Material.BEACON, 1);
 				prizeName = "A Beacon!";
 				PlayerInventory inventory = player.getInventory();
 				inventory.addItem(itemstack);
 				break;
 			}
-			case 5: {
+			case 5:
+			{
 				itemstack = new ItemStack(Material.ANVIL);
 				prizeName = "Anvil";
 				PlayerInventory inventory = player.getInventory();
 				inventory.addItem(itemstack);
 				break;
 			}
-			case 6: {
+			case 6:
+			{
 				Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "eco give " + 
 				player.getName() + " 1000");
 				prizeName = "$1000";
 				break;
 				// Add money
 			}
-			case 7: {
+			case 7:
+			{
 				Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "eco give " + 
 				player.getName() + " 1000");
 				prizeName = "$1000";
 				break;
 				// Add money
 			}
-			case 9: {
+	    case 8:
+	    {
+	      itemstack = new ItemStack(Material.IRON_SWORD, 1);
+	      prizeName = "Iron Sword";
+	      PlayerInventory inventory = player.getInventory();
+	      inventory.addItem(itemstack);
+	      break;
+	    }
+			case 9:
+			{
 				Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "eco give " + 
 				player.getName() + " 1000");
 				prizeName = "$1000";
 				break;
 				// Add money
 			}
-			case 11: {
+      case 10:
+      {
+        itemstack = new ItemStack(Material.DIAMOND_SWORD, 1);
+        prizeName = "Diamond Sword";
+        PlayerInventory inventory = player.getInventory();
+        inventory.addItem(itemstack);
+        break;
+      }
+			case 11:
+			{
 				Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "eco give " + 
 				player.getName() + " 1000");
 				prizeName = "$1000";
 				break;
 				// Add money
 			}
-			case 12: {
+			case 12:
+			{
 				Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "eco give " + 
 				player.getName() + " 1000");
 				prizeName = "$1000";
 				break;
 				// Add money
 			}
-			case 13: {
+			case 13:
+			{
 				Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "eco give " + 
 				player.getName() + " 1000");
 				prizeName = "$1000";
 				break;
 				// Add money
 			}
-			case 14: {
+			case 14:
+			{
 				Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "eco give " + 
 				player.getName() + " 1000");
 				prizeName = "$1000";
 				break;
 				// Add money
 			}
-			case 15: {
+			case 15:
+			{
+	      Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "tokens give " + 
+	      player.getName() + " 100");
+	      prizeName = "$100 Tokens!";
 			  break;
 				// Add tokens
 			}
-			case 16: {
-			  break;
-				// Add tokens
+			case 16:
+			{
+        Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "tokens give " + 
+        player.getName() + " 50");
+        prizeName = "$50 Tokens!";
+        break;
+        // Add tokens
 			}
-			case 17: {
-			  break;
-				// Add tokens
+			case 17:
+			{
+        Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "tokens give " + 
+        player.getName() + " 25");
+        prizeName = "$25 Tokens!";
+        break;
+        // Add tokens
 			}
-			case 18: {
-			  break;
-				// Add tokens
+			case 18:
+			{
+        Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "tokens give " + 
+        player.getName() + " 10");
+        prizeName = "$100 Tokens!";
+        break;
+        // Add tokens
 			}
-			case 19: {
-			  break;
-				// Add tokens
+			case 19:
+			{
+        Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "tokens give " + 
+        player.getName() + " 10");
+        prizeName = "$10 Tokens!";
+        break;
+        // Add tokens
 			}
-			case 20: {
+			case 20:
+			{
 				Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "eco give " + 
 				player.getName() + " 1000");
 				prizeName = "$1000";
 				break;
 				// Add McMMO credits
 			}
-			case 21: {
+			case 21:
+			{
 			  break;
 				// Add McMMO credits
 			}
-			case 22: {
+			case 22:
+			{
 			  break;
 				// Add McMMO credits
 			}
-			case 23: {
+			case 23:
+			{
 			  break;
 				// Add McMMO credits
 			}
-			case 24: {
+			case 24:
+			{
 				itemstack = new ItemStack(Material.COAL_ORE, 16);
 				prizeName = "Coal Ore";
 				PlayerInventory inventory = player.getInventory();
 				inventory.addItem(itemstack);
 				break;
 			}
-			case 25: {
+			case 25:
+			{
 				itemstack = new ItemStack(Material.DIAMOND_ORE, 8);
 				prizeName = "Diamond Ore";
 				PlayerInventory inventory = player.getInventory();
 				inventory.addItem(itemstack);
 				break;
 			}
-			case 26: {
+			case 26:
+			{
 				itemstack = new ItemStack(Material.GLOWSTONE_DUST, 16);
 				prizeName = "Glowstone!";
 				PlayerInventory inventory = player.getInventory();
 				inventory.addItem(itemstack);
 				break;
 			}
-			case 27: {
+			case 27:
+			{
+			  itemstack = new ItemStack(Material.BLAZE_ROD, 5);
+			  prizeName = "Blazkerods!";
+			  PlayerInventory inventory = player.getInventory();
+			  inventory.addItem(itemstack);
 			  break;
 			}
-			case 28: {
+			case 28:
+			{
+        itemstack = new ItemStack(Material.BLAZE_ROD, 5);
+        prizeName = "Blazkerods!";
+        PlayerInventory inventory = player.getInventory();
+        inventory.addItem(itemstack);
+        break;
+			}
+			case 29:
+			{
+        itemstack = new ItemStack(Material.BLAZE_ROD, 5);
+        prizeName = "Blazkerods!";
+        PlayerInventory inventory = player.getInventory();
+        inventory.addItem(itemstack);
+        break;
+			}
+			case 30:
+			{
+			  itemstack = new ItemStack(Material.CHAINMAIL_BOOTS, 1);
+			  prizeName = "Chainmail Boots!";
+			  PlayerInventory inventory = player.getInventory();
+			  inventory.addItem(itemstack);
 			  break;
 			}
-			case 29: {
+			case 31:
+			{
+			  itemstack = new ItemStack(Material.CHAINMAIL_CHESTPLATE, 1);
+			  prizeName = "Chainmail Chestplate!";
+			  PlayerInventory inventory = player.getInventory();
+			  inventory.addItem(itemstack);
 			  break;
 			}
-			case 30: {
+			case 32:
+			{
+			  itemstack = new ItemStack(Material.CHAINMAIL_HELMET, 1);
+			  prizeName = "Chainmail Helmet!";
+			  PlayerInventory inventory = player.getInventory();
+			  inventory.addItem(itemstack);
 			  break;
 			}
-			case 31: {
+			case 33:
+			{
+			  itemstack = new ItemStack(Material.CHAINMAIL_LEGGINGS, 1);
+			  prizeName = "Chainmail Leggings!";
+			  PlayerInventory inventory = player.getInventory();
+			  inventory.addItem(itemstack);
 			  break;
 			}
-			case 32: {
+			case 34:
+			{
+			  itemstack = new ItemStack(Material.CAKE, 1);
+			  prizeName = "Cake";
+			  PlayerInventory inventory = player.getInventory();
+			  inventory.addItem(itemstack);
 			  break;
 			}
-			case 33: {
+			case 35:
+			{
+			  itemstack = new ItemStack(Material.DIAMOND_SWORD, 1);
+			  itemstack.addEnchantment(SHARPNESS, 3);
+			  prizeName = "Sharp Diamond Sword!";
+			  PlayerInventory inventory = player.getInventory();
+			  inventory.addItem(itemstack);
 			  break;
 			}
-			case 34: {
+			case 36:
+			{
+			  
 			  break;
 			}
-			case 35: {
+			case 37:
+			{
+			  
 			  break;
 			}
-			case 36: {
+			case 38:
+			{
+			  
 			  break;
 			}
-			case 37: {
+			case 39:
+			{
+			  
 			  break;
 			}
-			case 38: {
+			case 40:
+			{
+			  
 			  break;
 			}
-			case 39: {
+			case 41:
+			{
+			  
 			  break;
 			}
-			case 40: {
+			case 42:
+			{
+			  
 			  break;
 			}
-			case 41: {
+			case 43:
+			{
+			  
 			  break;
 			}
-			case 42: {
+			case 44:
+			{
+			  
 			  break;
 			}
-			case 43: {
+			case 45:
+			{
+			  
 			  break;
 			}
-			case 44: {
+			case 46:
+			{
+			  
 			  break;
 			}
-			case 45: {
+			case 47:
+			{
+			  
 			  break;
 			}
-			case 46: {
+			case 48:
+			{
+			  
 			  break;
 			}
-			case 47: {
+			case 49:
+			{
+			  
 			  break;
 			}
-			case 48: {
+			case 50:
+			{
+			  
 			  break;
 			}
-			case 49: {
-			  break;
-			}
-			case 50: {
-			  break;
-			}
-			
-			
-			case 10: {
-				itemstack = new ItemStack(Material.DIAMOND_SWORD, 1);
-				prizeName = "Diamond Sword";
-				PlayerInventory inventory = player.getInventory();
-				inventory.addItem(itemstack);
-				break;
-			}
-			case 8: {
-				itemstack = new ItemStack(Material.IRON_SWORD, 1);
-				prizeName = "Iron Sword";
-				PlayerInventory inventory = player.getInventory();
-				inventory.addItem(itemstack);
-				break;
-			}
-			default: {
+			default:
+			{
 				itemstack = new ItemStack(Material.EXP_BOTTLE, 32);
 				prizeName = "Bottles of Enchanting";
 				PlayerInventory inventory = player.getInventory();
@@ -282,19 +405,13 @@ public class Main extends JavaPlugin {
 				break;
 			}
 			}
-			
-			for (Player p : Bukkit.getOnlinePlayers()) {
-				p.sendMessage(ChatColor.GOLD + "[Swag-Raffles]" + ChatColor.AQUA + " " + player.getName() + " has won " + ChatColor.BOLD + prizeName + ChatColor.AQUA + " from /raffles!");
-			}
-			
-			// This was the initial idea for making the raffles
-			//int randomNumber;
-			// This creates a random number from 0 --> 1000
-			//randomNumber = (int)(Math.random() * 10000);
-			//if (randomNumber <= 100 && randomNumber >= 0) {
-				// 9.9% chance
-			//}
-			
+		 
+			for (Player p : Bukkit.getOnlinePlayers())
+			{
+				p.sendMessage(ChatColor.GOLD + "[Swag-Raffles]" + ChatColor.AQUA + " "
+				    + "" + player.getName() + " has won " + ChatColor.BOLD + 
+				    prizeName + ChatColor.AQUA + " from /raffles!");
+			}			
 			return true;
 		}
 		return false;
